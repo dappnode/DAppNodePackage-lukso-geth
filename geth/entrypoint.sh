59f9ce1d2c929139bf5a9676dd5b8de9bf9ac4d4
Diego <dsimog01@estudiantes.unileon.es>
2023-07-13 18:12:35 +0200

    Updated pkg for production

---
+++ geth/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO" in
+ "prysm-lukso.dnp.dappnode.eth")
+   echo "Using prysm-lukso.dnp.dappnode.eth"
+   JWT_PATH="/security/prysm/jwtsecret.hex"
+   ;;
+ "lighthouse-lukso.dnp.dappnode.eth")
+   echo "Using lighthouse-lukso.dnp.dappnode.eth"
+   JWT_PATH="/security/lighthouse/jwtsecret.hex"
+   ;;
+ *)
+   echo "Using default JWT"
+   JWT_PATH="/security/default/jwtsecret.hex"
+   ;;
+esac
+
+# Print the jwt to the dappmanager
+JWT=$(cat $JWT_PATH)
+curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
+
+# Init Geth from genesis
+exec geth --datadir=/lukso init /configs/genesis.json
+
+exec geth \
+  --datadir=/lukso \
+  --ws \
+  --ws.api eth,engine,net,web3,txpool \
+  --ws.addr 0.0.0.0 \
+  --ws.origins "*" \
+  --http \
+  --http.api eth,engine,net,web3,txpool \
+  --http.addr 0.0.0.0 \
+  --http.corsdomain "*" \
+  --http.vhosts "*" \
+  --ipcdisable \
+  --authrpc.addr 0.0.0.0 \ \
+  --authrpc.port 8551 \
+  --authrpc.vhosts "*" \
+  --authrpc.jwtsecret ${JWT_PATH} \
+  --metrics \
+  --metrics.addr 0.0.0.0 \
+  --port ${P2P_PORT} \
+  ${EXTRA_FLAGS}
+#  --bootnodes $EXECUTION_BOOTSTRAP_NODE_1 \
+#  --networkid 42  \
+#  --miner.gaslimit 42000000 \
+#  --miner.gasprice 4200000000 \
