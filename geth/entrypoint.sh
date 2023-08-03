3aa44ab4064d25ff77337cabfd7c8b452136539c
dsimog01 <47595345+dsimog01@users.noreply.github.com>
2023-08-03 13:54:53 +0200

    Remove lukso config file (#5)

--- geth/entrypoint.sh
+++ geth/entrypoint.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO" in
-  "prysm-lukso.dnp.dappnode.eth")
-    echo "Using prysm-lukso.dnp.dappnode.eth"
-    JWT_PATH="/security/prysm/jwtsecret.hex"
-    ;;
-  "lighthouse-lukso.dnp.dappnode.eth")
-    echo "Using lighthouse-lukso.dnp.dappnode.eth"
-    JWT_PATH="/security/lighthouse/jwtsecret.hex"
-    ;;
-  "lodestar-lukso.dnp.dappnode.eth")
-      echo "Using lodestar-lukso.dnp.dappnode.eth"
-      JWT_PATH="/security/lodestar/jwtsecret.hex"
-      ;;
-  "teku-lukso.dnp.dappnode.eth")
-      echo "Using teku-lukso.dnp.dappnode.eth"
-      JWT_PATH="/security/teku/jwtsecret.hex"
-      ;;
-  "nimbus-lukso.dnp.dappnode.eth")
-      echo "Using nimbus-lukso.dnp.dappnode.eth"
-      JWT_PATH="/security/nimbus/jwtsecret.hex"
-      ;;
-  *)
-    echo "Using default JWT"
-    JWT_PATH="/security/default/jwtsecret.hex"
-    ;;
+"prysm-lukso.dnp.dappnode.eth")
+  echo "Using prysm-lukso.dnp.dappnode.eth"
+  JWT_PATH="/security/prysm/jwtsecret.hex"
+  ;;
+"lighthouse-lukso.dnp.dappnode.eth")
+  echo "Using lighthouse-lukso.dnp.dappnode.eth"
+  JWT_PATH="/security/lighthouse/jwtsecret.hex"
+  ;;
+"lodestar-lukso.dnp.dappnode.eth")
+  echo "Using lodestar-lukso.dnp.dappnode.eth"
+  JWT_PATH="/security/lodestar/jwtsecret.hex"
+  ;;
+"teku-lukso.dnp.dappnode.eth")
+  echo "Using teku-lukso.dnp.dappnode.eth"
+  JWT_PATH="/security/teku/jwtsecret.hex"
+  ;;
+"nimbus-lukso.dnp.dappnode.eth")
+  echo "Using nimbus-lukso.dnp.dappnode.eth"
+  JWT_PATH="/security/nimbus/jwtsecret.hex"
+  ;;
+*)
+  echo "Using default JWT"
+  JWT_PATH="/security/default/jwtsecret.hex"
+  ;;
 esac
 
 echo "[INFO - entrypoint] Posting JWT to dappmanager: ${JWT_PATH}"
@@ -36,7 +36,11 @@ geth --datadir=/lukso init /config/genesis.json
 
 echo "[INFO - entrypoint] Starting Geth"
 exec geth --datadir /lukso \
-  --config /config/geth.toml \
+  --networkid 42 \
+  --miner.gasprice 4200000000 \
+  --miner.gaslimit 42000000 \
+  --bootnodes enode://c2bb19ce658cfdf1fecb45da599ee6c7bf36e5292efb3fb61303a0b2cd07f96c20ac9b376a464d687ac456675a2e4a44aec39a0509bcb4b6d8221eedec25aca2@34.147.73.193:30303", "enode://276f14e4049840a0f5aa5e568b772ab6639251149a52ba244647277175b83f47b135f3b3d8d846cf81a8e681684e37e9fc10ec205a9841d3ae219aa08aa9717b@34.32.192.211:30303 \
+  --maxpeers 50 \
   --port ${P2P_PORT} \
   --http \
   --http.api eth,engine,net,web3,txpool \
